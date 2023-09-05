13f6816733aa4317000922fee9568f548ae2cf30
Arpit <omagrahari55@gmail.com>
2023-09-05 20:37:29 +0530

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,10 +22,10 @@ const Header = () => {
         <h4 className='logoName'>CodeBro</h4>
         <main className='main'>
         <img className='close'  onClick={closefun} src={close} alt="" />
-            <Link  className='nava' onClick={closefun} to="/">Home</Link>
-            <HashLink className='nava' onClick={closefun} to="/#About">About</HashLink>
-            <Link className='nava' onClick={closefun} to="/Contact">Contact</Link>
-            <Link className='nava' onClick={closefun} to="/Course">Course</Link>
+            <Link  className='nava'  to="/">Home</Link>
+            <HashLink className='nava'  to="/#About">About</HashLink>
+            <Link className='nava'  to="/Contact">Contact</Link>
+            <Link className='nava'  to="/Course">Course</Link>
         </main>
         <img onClick={menuFun}  className="menu" src={menu} alt="" />
     </nav>
